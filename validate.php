755aaa5c7757825313c63ea50b1b28aaf716e1ed
powerhouse1011 <knox0477@gmail.com>
2024-10-26 13:53:22 -0500

    Update validate.php

--- validate.php
+++ validate.php
@@ -1,16 +1,15 @@
 <?php
-// Get the password submitted via the form
+// validate.php
 $password = $_POST['password'];
 
-// Check the password and redirect based on its value
 if ($password == '738578') {
     header('Location: http://powerhouse1011.github.io/home');
     exit;
 } elseif ($password == 'freesongs') {
-    header('Location: https://powerhouse1011.github.io/freesongs');
+    header('Location: http://powerhouse1011.github.io/freesongs');
     exit;
 } else {
-    header('Location: https://NOPE.github.io');
+    header('Location: http://ww.example.com');
     exit;
 }
 ?>
