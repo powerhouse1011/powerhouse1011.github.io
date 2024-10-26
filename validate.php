06cbd575b524e91fb0d945f82e8e486bb01c55da
powerhouse1011 <knox0477@gmail.com>
2024-10-26 13:13:30 -0500

    Update validate.php

--- validate.php
+++ validate.php
@@ -10,7 +10,6 @@ if ($password == '738578') {
     header('Location: https://powerhouse1011.github.io/freesongs');
     exit;
 } else {
-    // Redirect to an error page or back to login if the password is incorrect
     header('Location: https://NOPE.github.io');
     exit;
 }
